06422bc3eeff9edaaa681b550fc7ab8e37f39b79
kaartav <aavulakaartavaraya@gmail.com>
2024-04-19 17:03:00 +0530

    ch

--- decision_trees_func.py
+++ decision_trees_func.py
@@ -2,6 +2,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
+import pandas as pd
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
 
 def evaluate_model(y_true, y_pred):
@@ -41,4 +42,17 @@ def decision_tree_model(df, dep_variable ,test_size=0.2, random_state=42):
     evaluation_metrics = evaluate_model(y_test, y_pred)
 
     return decision_tree_model, evaluation_metrics
+df=pd.read_csv("data/FINAL_DATA_FOR_REG.csv")
+df.dropna(inplace=True)
 
+# decision_tree_model, evaluation_metrics=decision_tree_model(df,"Hospital delivery")
+# print(evaluation_metrics)
+# {'Confusion_Matrix': array([[175,   0],
+#        [  1, 189]]), 'Accuracy': 0.9972602739726028, 'Precision': 0.9972758405977584, 'Recall': 0.9972602739726028, 'F1-Score': 0.9972605623019475}
+# decision_tree_model, evaluation_metrics=decision_tree_model(df,"PrenatalCare")
+# print(evaluation_metrics)
+# {'Confusion_Matrix': array([[365]]), 'Accuracy': 1.0, 'Precision': 1.0, 'Recall': 1.0, 'F1-Score': 1.0}
+decision_tree_model, evaluation_metrics=decision_tree_model(df,"Child is alive")
+print(evaluation_metrics)
+# {'Confusion_Matrix': array([[  1,   8],
+#        [ 15, 341]]), 'Accuracy': 0.936986301369863, 'Precision': 0.9545261412254191, 'Recall': 0.936986301369863, 'F1-Score': 0.9454953852132518}
